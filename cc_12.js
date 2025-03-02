a19551c46b902dbe8e4c81494b1a8546f7973de3
mallikanicole <malmichael23@gmail.com>
2025-03-01 22:52:36 -0500

    test task 1

---
+++ cc_12.js
@@ -0,0 +1,4 @@
+//Task 1  Business Dashboard – DOM Element Selection and Creation
+
+const elementID=document.getElementById('dashboard');
+const dashboardQuery=document.querySelector('.dashboard');
