9105c81c76828965018b8226d2375a6ee2594549
mallikanicole <malmichael23@gmail.com>
2025-03-01 23:03:23 -0500

    test task 1-a

--- cc_12.js
+++ cc_12.js
@@ -2,3 +2,11 @@
 
 const elementID=document.getElementById('dashboard');
 const dashboardQuery=document.querySelector('.dashboard');
+const revenueCard=document.createElement('div');
+revenueCard.setAttribute("class","metric-card");
+revenueCard.setAttribute("id","revenue-card");
+revenueCard.innerHTML=`
+<h3>Revenue</h3>
+<p>$0</p>
+`
+dashboardID.appendChild(revenueCard);
